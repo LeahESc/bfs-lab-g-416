2f25b7a4fc8d6ffbc343f4466f4da8e49bf630ed
LeahESc <leahemily1@gmail.com>
2021-02-27 09:50:36 -0800

    commit tree c8ee2c19bf7c712e2594eb2dd358977ea624b5a0

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function bfs(rootNode, vertices, edges){
+    // rootNode = vertices[0]
+    // let queue = []
+    // vertices.shift()
+    // while(vertices.length != 0) {
+        
+    // }
+}
 
+function findAdjacent(vertices, edges, nodeName){
+    return edges.filter(edge => {
+        return edge.includes(nodeName)
+    }).map( edge => {
+        return edge.filter(node => {
+            return (node != nodeName)
+        })[0]
+    }).map(name => {
+        return findNode(name, vertices)
+    }).filter(node => node.distance == null)  
+}
+
+function findNode(nodeName, vertices){
+    return vertices.find(vertex => vertex.name == nodeName)
 }
